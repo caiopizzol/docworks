00ae0ba11907d8839aebd5393d6e0c6cf300db64
Caio Pizzol <caiopizzol@icloud.com>
2025-11-10 14:49:03 -0300

    chore: update commitlint configuration to disable line length rules

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [0, 'always'],
+    'body-max-line-length': [0, 'always'],
+    'footer-max-line-length': [0, 'always'],
+  },
 }
